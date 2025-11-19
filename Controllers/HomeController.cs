ca3abb86676a472f8a9364807517ff504baad9ed
wagnerrocha20021409-jpg <wagner.rocha20021409@gmail.com>
2025-11-18 21:52:19 -0300

    segundo comentario

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,7 @@ public class HomeController : Controller
     {
         return View();
     }
-
+        // Adicionar um comentario
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
