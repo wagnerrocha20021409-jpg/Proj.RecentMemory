0273a9fad1f045c29815cdeecfacef3617bc3e50
PRISCILA LUZOLO DA SILVA JOAO <luzolopriscila720@gmail.com>
2025-11-26 21:54:12 -0300

    Atualização Pagina inicial

--- obj/Debug/net9.0/RecentMemory.AssemblyInfo.cs
+++ obj/Debug/net9.0/RecentMemory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecentMemory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb79bec91ac0645491cf033995b19960bba9925d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04fec5b50d3e2f1141e942247276e70492010623")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecentMemory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecentMemory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
